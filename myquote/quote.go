34353a473ae6aecfbcd22f593d0adc915c626b84
Magnus <magnusen@uia.no>
2022-01-28 21:39:22 +0100

    Ferdigstillt myquote

--- myquote/quote.go
+++ myquote/quote.go
@@ -5,3 +5,15 @@ import "rsc.io/quote"
 func Glass() string {
 	return quote.Glass()
 }
+
+func Go() string {
+	return quote.Go()
+}
+
+func Hello() string {
+	return quote.Hello()
+}
+
+func Opt() string {
+	return quote.Opt()
+}
